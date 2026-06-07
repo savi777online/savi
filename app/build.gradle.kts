aa0aa2bd1a16e511c0245713a3d2a6400b39631f
savi777online <savi777online@gmail.com>
2026-06-07 11:13:33 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.savi.app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.savi.app"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+}
